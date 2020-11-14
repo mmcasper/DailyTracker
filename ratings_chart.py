e8ef40a3009a036b26c11047fb155dce31159e85
Meghan Casper <casper.sriracha@gmail.com>
2020-11-14 17:30:09 -0500

    Playing with extra features: date check and rating graph

---
+++ ratings_chart.py
@@ -0,0 +1,34 @@
+import csv
+
+#from matplotlib import pyplot as plt
+
+filename = 'users.csv'
+
+#create function to read csv file
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    #if you wanted to see each index and data for header column
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+
+
+
+#create ratings list to store data from each row
+ratings = []
+for row in reader:
+    ratings.append(row[5])   #append daily rating from each row
+print(ratings)
+
+
+
+#convert strings to int and ignore empty strings
+#for row in reader:
+    #if row[5] == '':        #if empty string, continue itteratin
+        #continue
+    #rating = int(row[5])   #change string to int
+    #ratings.append(rating)       #add rating to ratings list
+
+#print(ratings)
