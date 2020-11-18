dc567598e0fc2a14c33bc7701ebfdc9ee4aa96a8
Meghan Casper <casper.sriracha@gmail.com>
2020-11-18 13:58:19 -0500

    cleaned up comments and removed buggy line of code from date input

--- data2.py
+++ data2.py
@@ -9,41 +9,41 @@ def append_dict_as_row(file_name, dict_of_elem, field_names):
     with open(file_name, 'a+', newline='') as write_obj:
     #Create a writer object from csv module
         dict_writer = DictWriter(write_obj, fieldnames=field_names)
-    #Add dict as wor in the csv
+    #Add dict as row in the csv
         dict_writer.writerow(dict_of_elem)
 
 #function to say sign off
 def goodbye():
-    print('Thank you for checking in today!')
+    print('Thank you for checking in today {}!'.format(name))
 
-#create welcome and exit to leave the program
+#create welcome 
+print("Welcome to your daily mood tracker. You will be asked for a daily rating after recording events and activities that may have affected that rating.")
 
 #collect fields from user
-
-username = input('Please create a username without any spaces: ') + (',')
+username = input('Please create a username without any spaces: ')
 #test for whitespace
-name = input('What is your first name? ') + (',')
-date = input('What is today\'s date? Please follow the mm/dd/yyyy format. ') + (',')
-        #test for correct formatting of date
-answer = True
-while answer == True:
-    date = input('Please enter the date in the mm/dd/yyyy format?. ') + (',')
+
+name = input('What is your first name? ')
+
+#Use regex to test for correct formatting of date
+date_answer = True
+while date_answer == True:
+    date = input('Please enter the date in the mm/dd/yyyy format?. ')
 
     if not re.match(r'(\d{2}(\/)\d{2}(\/)\d{4})', date):
         print('Please follow the correct format for date: mm/dd/yyyy. ')
-        answer = True
+        date_answer = True
     else:
         print('The date is: ' + date)
-        answer = False
+        date_answer = False
 
-
-
-journal = input('Please enter any thoughts you would like to record any thoughts for this day? ') + (',')
+journal = input('Please enter any thoughts you would like to record any thoughts for this day? ')
 #create empty string for no
 
-activity = input('Please record any physical activity for the day? ') + (',')
+activity = input('Please record any physical activity for the day? ')
 #create empty string for no
 
+#Use regex to test for requested rating input
 answer = True
 while answer == True:
     rating = input('What is your rating for this day on a scale of 1-10? ')
@@ -55,8 +55,10 @@ while answer == True:
         print('Your rating is: ' + rating)
         answer = False
 
+#Create field names and assign input to that field
 field_names = ['Username', 'Name', 'Date', 'Daily Journal', 'Activity Log', 'Daily Rating']
-row_dict = {'Username' : '{}'.format(username), 'Name' : ' {}'.format(name), 'Date' : ' {}'.format(date), 'Daily Journal' : ' {}'.format(journal),'Activity Log' : ' {}'.format(activity),'Daily Rating' : ' {}'.format(rating)}
+row_dict = {'Username' : '{},'.format(username), 'Name' : ' {},'.format(name), 'Date' : ' {},'.format(date), 'Daily Journal' : ' {},'.format(journal),'Activity Log' : ' {},'.format(activity),'Daily Rating' : ' {}'.format(rating)}
+
 #Using function to write to csv file.
 append_dict_as_row('users.csv', row_dict, field_names)
 
